5cf4f50057b54e7d9840e42ec4efb4ca4b1243b9
Oscar Deb <20-42888-1@student.aiub.edu>
2025-05-27 01:17:19 +0600

    changes added

--- app/components/Header/header.js
+++ app/components/Header/header.js
@@ -1,28 +1,23 @@
 "use client";
-import Link from "next/link"
-import './header.css';
+import Link from "next/link";
+import "./header.css";
 import { useState } from "react";
 //import MobileNav from "../MobileNav/MobileNav";
 const Header = () => {
-<link rel="icon" 
-href="/logo.png" 
-type="image"
-
-/>
+  <link rel="icon" href="/logo.png" type="image" />;
   // const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
   // const handleMobileNavClick = () => {
   //   setIsMobileNavOpen(!isMobileNavOpen);
-  // }; 
+  // };
 
   // const DriveLink = () => (
   //     <Link href="https://facebook.com">
   //     </Link>
   //  );
-  
-  
+
   return (
-       <header className="header container">
+    <header className="header container">
       {/* <link rel="icon" 
 href="./logo.icon" 
 type="image"
@@ -31,13 +26,19 @@ type="image"
       <nav>
         <ul className="header__menu">
           <li>
-            <a className="header__link" href="#about">About</a>
+            <a className="header__link" href="#about">
+              About
+            </a>
           </li>
           <li>
-            <a className="header__link" href="#featured">Work</a>
+            <a className="header__link" href="#featured">
+              Work
+            </a>
           </li>
           <li>
-            <a className="header__link" href="#contact">Contact</a>
+            <a className="header__link" href="#contact">
+              Contact
+            </a>
           </li>
           <li className="header__line"></li>
           {/* <li>
@@ -58,36 +59,44 @@ type="image"
             </button>
           </li> */}
           <li>
-            <a className="header__resume btn" href="https://drive.google.com/file/d/1pbmoIH6oQUoUVF8Mtp4LO0-dxi1RLoOP/view?usp=sharing" target="_blank">Resume</a>
-         
-          
+            <a
+              className="header__resume btn"
+              href="https://drive.google.com/file/d/1pbmoIH6oQUoUVF8Mtp4LO0-dxi1RLoOP/view?usp=sharing"
+              target="_blank"
+            >
+              Resume
+            </a>
+
             {/* <Link className="header__resume btn" href="https://facebook.com" target="_blank">Resume</Link>
-          */}
+             */}
           </li>
         </ul>
-        <button aria-label="mobile nav button" className="header__bars"
-        // onClick={handleMobileNavClick}
-        href="https://drive.google.com/file/d/1ZWSvKmxWQsS7T55qfkH-0Z_fc7Zio1mj/view?usp=sharing" target="_blank"
-        //onClick={DriveLink}       
-         >
-           <svg
+        <button
+          aria-label="mobile nav button"
+          className="header__bars"
+          // onClick={handleMobileNavClick}
+          href="https://drive.google.com/file/d/1ZWSvKmxWQsS7T55qfkH-0Z_fc7Zio1mj/view?usp=sharing"
+          target="_blank"
+          //onClick={DriveLink}
+        >
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
           >
             <path
               fillRule="evenodd"
-             // fill-rule="evenodd"
+              // fill-rule="evenodd"
               d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
               clipRule="evenodd"
             />
           </svg>
-         </button> 
+        </button>
       </nav>
-      
+
       {/* {isMobileNavOpen && <MobileNav />} */}
     </header>
-    );
-}
+  );
+};
 
 export default Header;
--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 import Link from "next/link";
-import  {useEffect} from 'react';
-import mNav from './components/JsContent/MNav';
-import lazyLoading from './components/JsContent/LazyLoading';
+import { useEffect } from "react";
+import mNav from "./components/JsContent/MNav";
+import lazyLoading from "./components/JsContent/LazyLoading";
 
 //import Image from 'next/image'
 //import styles from './page.module.css'
-import Hero from './components/Hero/Hero'
-import About from './components/About/About'
-import Featured from './components/Featured/Featured'
-import Work from './components/Work/Work'
-import Contact from './components/Contact/Contact'
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Featured from "./components/Featured/Featured";
+import Work from "./components/Work/Work";
+import Contact from "./components/Contact/Contact";
 
 export default function Home() {
- 
   useEffect(() => {
     mNav();
     lazyLoading();
@@ -21,15 +20,12 @@ export default function Home() {
 
   return (
     // <main className={styles.main}>
-     <main >
-
-
-<Hero />
-<About />
-<Featured />
-<Work />
-<Contact />
-
-       </main>
-  )
+    <main>
+      <Hero />
+      <About />
+      <Featured />
+      <Work />
+      <Contact />
+    </main>
+  );
 }
